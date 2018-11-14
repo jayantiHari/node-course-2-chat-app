e89897a8a0e09aa4f7bc3407123e705102207c67
jayantiHari <jayantihari6@gmail.com>
2018-11-14 15:00:37 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function() {
   console.log('Connected to server');
 
-  socket.emit('createMessage', {
-    from: 'jayanti',
-    text: 'hello'
-  });
 });
 
 socket.on('disconnect', function() {
--- server/server.js
+++ server/server.js
@@ -17,14 +17,15 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
-  socket.emit('newMessage', {
-    from: 'Andrew',
-    text: 'See you soon'
-  });
 
-  socket.on('createMessage', (input) => {
-    console.log('createMessage', input);
 
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+      io.emit('newMessage', {
+        from: message.from,
+        text: message.text,
+        createdAt: new Date().getTime()
+      })
   });
 
 
